2046b61478a3835dfade92aa925012d5fb76c941
styg-git <taylorjonesla@gmail.com>
2025-05-28 01:27:40 -0500

    Updated structure of project. Created device management class. Integrated PowerShell's AudioDeviceCmdLets.

--- audio_controller.py
+++ audio_controller.py
@@ -1,7 +1,9 @@
 from pycaw.api.endpointvolume import IAudioEndpointVolume
-from pycaw.pycaw import AudioUtilities
+from pycaw.pycaw import AudioUtilities, AudioDevice, EDataFlow, ERole
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
+import subprocess
+
 
 def list_audio_sessions():
     sessions = AudioUtilities.GetAllSessions()
@@ -9,9 +11,11 @@ def list_audio_sessions():
         if session.Process:
             print(f"{session.Process.name()}")
 
+
 def list_output_device_type():
     device = AudioUtilities.GetSpeakers()
     print(f"Got default output device object: {device}")
+
 
 def print_system_volume():
     devices = AudioUtilities.GetSpeakers()
@@ -19,3 +23,14 @@ def print_system_volume():
     volume = cast(interface, POINTER(IAudioEndpointVolume))
     current_volume = volume.GetMasterVolumeLevelScalar()
     print(f"Current system volume: {round(current_volume * 100)}%")
+
+
+def list_audio_devices():
+    cmd = ['powershell', '-Command', 'Get-AudioDevice -List']
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    return result.stdout.strip().splitlines()
+
+
+def set_audio_device(name):
+    cmd = ['powershell', '-Command', f'Set-AudioDevice -Name "{name}"']
+    subprocess.run(cmd)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from audio_controller import list_audio_sessions, list_output_device_type, print_system_volume
+from core.audio_device_manager import AudioDeviceManager
+from audio_controller import print_system_volume  # just this one for now
 
 if __name__ == "__main__":
-    print("Active Audio Sessions:")
-    list_audio_sessions()
-    list_output_device_type()
-    print_system_volume()
+    print("\nAvailable Output Devices:")
+    manager = AudioDeviceManager()
+    devices = manager.list_devices()
 
+    for i, device in enumerate(devices):
+        print(f"{i + 1}. {device}")
+
+    current = manager.get_current_device()
+    print(f"\nCurrent Audio Device: {current}\n")
+    print_system_volume()
